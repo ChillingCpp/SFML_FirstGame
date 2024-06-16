66e575adf714e3e792c906dab044f67200d4f883
ChillingCpp <168641797+ChillingCpp@users.noreply.github.com>
2024-06-16 11:49:07 +0700

    Delete SFML directory

--- SFML/Graphics/BoundingBox.hpp
+++
@@ -1,9 +0,0 @@
-
-#pragma once
-
-#include <SFML/Graphics/Capsule.hpp>
-#include <SFML/Graphics/Obb.hpp>
-#include <SFML/Graphics/Circle.hpp>
-#include <SFML/Graphics/Rect.hpp>
-#include <SFML/Graphics/Convexhull.hpp>
-#include <SFML/Graphics/LineSegment.hpp>
